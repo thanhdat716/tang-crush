14139ac585a59d031ed1e99e3718fdaddc8dfeb6
thanhdat716 <85330979+thanhdat716@users.noreply.github.com>
2021-06-04 13:05:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,16 @@ const CONFIG = {
     Thích 1 ngườii là như thế nào?
     Tớ liền hình dung ra cậu🤭🙆‍♂️ `,
     btnIntro: 'hihi',
-    title: 'Tình yêu như là phép nhiệm màu cho nên đừng ngạii nếu uống say. Em à!🥰❤  ',
-    desc: 'Em có muốn cùng anh tạo nên 1 mối tình khôngg ?😊 ',
+    title: 'Tình yêu như là phép nhiệm màu cho nên đừng ngạii nếu uống say Em à 😘❤  ',
+    desc: 'Em có muốn cùng anh tạo nên 1 mối tình khôngg🙆‍♂️ ? ',
+    
     btnYes: 'iuu Đạt quá à 🙆‍♂️🙆‍♂️',
     btnNo: 'Cho cụng không thèmm 😗',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh zạyy 😘',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Tôi yêuu cậu không cần lý do🙆‍♂️❤ ',
+    reply: 'Tôi yêuu cậu không cần lý do :3 ',
     mess: 'Yêu em nhiều 😘😘',
-    messDesc: 'Xíu qua dẫn đii chơi nhaa🤭',
+    messDesc: 'Để xíuu công khaii ngiuu ha 🙆‍♂️❤',
     btnAccept: 'Dạaaaaaaa <333',
     messLink: 'https://m.me/thanhdat01012021'
 }
